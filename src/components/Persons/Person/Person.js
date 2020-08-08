163d3548d43969e608e7ceed5e1ff2f0d8bea51b
thebigbadox <adamwebb13@gmail.com>
2020-08-07 20:13:29 -0500

    Added Ref example

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,25 @@ import withClass from '../../../hoc/withClass'
 import classes from './Person.css'
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
             <Aux>
                 <p key='i1' onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key ='i2'>{this.props.children}</p>
-                <input key = 'i3' type="text" onChange={this.props.changed} value={this.props.name} />
+                <input key = 'i3' ref={this.inputElementRef} //ref={(inputEl) => {this.inputElement = inputEl;}} 
+                        type="text" onChange={this.props.changed} value={this.props.name} />
             </Aux>
         );
     }
